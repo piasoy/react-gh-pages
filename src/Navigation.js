46a1295ad8b4ccddc3f11d75ebe10f69767efc57
Pia Soy <piasoy4@gmail.com>
2020-02-21 17:19:31 -0600

    change nav links

--- src/Navigation.js
+++ src/Navigation.js
@@ -15,13 +15,13 @@ class Navigation extends React.Component {
                         <Nav.Link href="#link">How it works</Nav.Link>
                         <NavDropdown title="Wardrobes" id="basic-nav-dropdown">
                             <NavDropdown.Item href="/form">Work</NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.2">Maternity</NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.3">Weekend</NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.4">Seasonal</NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.5">Vacation</NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.6">Special Occasion</NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.2">Maternity - coming soon</NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.3">Weekend - coming soon</NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.4">Seasonal - coming soon</NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.5">Vacation - coming soon</NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.6">Special Occasion - coming soon</NavDropdown.Item>
                         </NavDropdown>
-                        <Nav.Link href="#link">Login</Nav.Link>
+                        {/* <Nav.Link href="#link">Login</Nav.Link> */}
                     </Nav>
                   
                 </Navbar.Collapse>
