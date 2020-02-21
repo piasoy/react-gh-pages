db07e0f3cd17f89613bc3e9dd913289172eeaf80
Pia Soy <piasoy4@gmail.com>
2020-02-21 15:36:55 -0600

    Updates

---
+++ precache-manifest.2cbada92692817a27e5f70c9b3c65cfd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66ecc9c7cf61bd284d6fb2b4d01f0e1a",
+    "url": "/react-gh-pages/index.html"
+  },
+  {
+    "revision": "05f872cc86c756b8f152",
+    "url": "/react-gh-pages/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "466df3b7dc5d3e46c8d8",
+    "url": "/react-gh-pages/static/css/main.84446651.chunk.css"
+  },
+  {
+    "revision": "05f872cc86c756b8f152",
+    "url": "/react-gh-pages/static/js/2.6113f36c.chunk.js"
+  },
+  {
+    "revision": "466df3b7dc5d3e46c8d8",
+    "url": "/react-gh-pages/static/js/main.3d71ce03.chunk.js"
+  },
+  {
+    "revision": "91e8848c25989c2ee7f1",
+    "url": "/react-gh-pages/static/js/runtime-main.ef53f737.js"
+  }
+]);
